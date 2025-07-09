f876a84ebeace59bfa5e354ec4535f00ab2c7656
Tharun <tharunpoongavanam@gmail.com>
2025-07-09 16:25:32 +0530

    Changes made

--- clustering_service.py
+++ clustering_service.py
@@ -154,4 +154,5 @@ def get_user(uid):
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
